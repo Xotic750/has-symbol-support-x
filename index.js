87d22c4fc63bd4e3607de80b5b0e465efccd7f59
Xotic750 <xotic750@gmail>
2015-12-01 17:20:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ * @file
+ * <a href="https://travis-ci.org/Xotic750/has-symbol-support-x"
+ * title="Travis status">
+ * <img
+ * src="https://travis-ci.org/Xotic750/has-symbol-support-x.svg?branch=master"
+ * alt="Travis status" height="18">
+ * </a>
+ * <a href="https://david-dm.org/Xotic750/has-symbol-support-x"
+ * title="Dependency status">
+ * <img src="https://david-dm.org/Xotic750/has-symbol-support-x.svg"
+ * alt="Dependency status" height="18"/>
+ * </a>
+ * <a
+ * href="https://david-dm.org/Xotic750/has-symbol-support-x#info=devDependencies"
+ * title="devDependency status">
+ * <img src="https://david-dm.org/Xotic750/has-symbol-support-x/dev-status.svg"
+ * alt="devDependency status" height="18"/>
+ * </a>
+ * <a href="https://badge.fury.io/js/has-symbol-support-x" title="npm version">
+ * <img src="https://badge.fury.io/js/has-symbol-support-x.svg"
+ * alt="npm version" height="18">
+ * </a>
+ *
+ * hasSymbolSupport module. Tests if Symbol and Symbol~iterator exist and are
+ * the correct types.
+ * @version 1.0.0
+ * @author Xotic750 <Xotic750@gmail.com>
+ * @copyright  Xotic750
+ * @license {@link <https://opensource.org/licenses/MIT> MIT}
+ * @module has-symbol-support-x
+ */
+
+/*jslint maxlen:80, es6:true, white:true */
+
+/*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
+  freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
+  nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
+  es3:true, esnext:true, plusplus:true, maxparams:1, maxdepth:1,
+  maxstatements:1, maxcomplexity:1 */
+
+/*global module */
+
+;(function () {
+  'use strict';
+
+  /**
+   * Indicates if Symbol and Symbol~iterator exist and are the correct types.
+   * `true`, if they exists and are the correct types, otherwise `false`.
+   *
+   * @type boolean
+   */
+  module.exports = typeof Symbol === 'function' &&
+    typeof Symbol.iterator === 'symbol';
+}());
---
+++ tests/spec/test.js
@@ -0,0 +1,29 @@
+/*jslint maxlen:80, es6:true, white:true */
+
+/*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
+  freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
+  nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
+  es3:true, esnext:true, plusplus:true, maxparams:1, maxdepth:1,
+  maxstatements:6, maxcomplexity:2 */
+
+/*global expect, module, require, describe, it, returnExports */
+
+(function () {
+  'use strict';
+
+  var hasSymbolSupport;
+  if (typeof module === 'object' && module.exports) {
+    require('es5-shim');
+    hasSymbolSupport = require('../../index.js');
+  } else {
+    hasSymbolSupport = returnExports;
+  }
+
+  describe('Basic tests', function () {
+    it('results should match', function () {
+      var expected = typeof Symbol === 'function' &&
+        typeof Symbol.iterator === 'symbol';
+      expect(hasSymbolSupport).toEqual(expected);
+    });
+  });
+}());

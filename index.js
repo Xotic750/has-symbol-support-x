4c772c704f14a07beb55a8e271d06c78b07b703a
Xotic750 <xotic750@gmail.com>
2017-09-01 17:33:39 +0200

    Fix lockfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Tests if ES6 Symbol is supported.
- * @version 1.4.0
+ * @version 1.4.1
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}

9bb3bfce11a39c3c303f1f4dd52155ec75902e8d
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:28:18 +0200

    :bookmark: v2.0.16
    Add jsdelivr

--- dist/has-symbol-support-x.js
+++ dist/has-symbol-support-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-23T23:47:53.303Z",
+  "date": "2019-07-24T13:28:13.168Z",
   "describe": "",
   "description": "Tests if ES6 Symbol is supported.",
   "file": "has-symbol-support-x.js",
   "hash": "ce9a22034841f6a2b175",
   "license": "MIT",
-  "version": "2.0.15"
+  "version": "2.0.16"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {

f309fbba5851e3cf4ec5ad02f598bc6393551b1f
Graham Fairweather <xotic750@gmail.com>
2019-07-25 23:40:55 +0200

    :bookmark: v2.0.18
    Auto build

--- dist/has-symbol-support-x.js
+++ dist/has-symbol-support-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-24T16:08:06.436Z",
+  "date": "2019-07-25T21:40:49.865Z",
   "describe": "",
   "description": "Tests if ES6 Symbol is supported.",
   "file": "has-symbol-support-x.js",
   "hash": "ce9a22034841f6a2b175",
   "license": "MIT",
-  "version": "2.0.17"
+  "version": "2.0.18"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
